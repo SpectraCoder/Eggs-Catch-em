a7fa7d69c49b7fd88eed7287797f6e13edadecda
Spectracoder <40742589+SpectraCoder@users.noreply.github.com>
2020-05-28 21:31:15 +0200

    Added some very basic code to let the eggs roll along the plank before they fall down.

--- Egg.js
+++ Egg.js
@@ -4,9 +4,9 @@ class Egg{constructor(canvasContext, image, chickenPositions)
         this.canvasContext = canvasContext;
         this.image = image;
         this.xPosition = randomChickenPosition.x + imageChicken.image.width / 2;
-        this.yPosition = randomChickenPosition.y + imageChicken.image.height;
-        //this.xPosition = getRandomInt(32, canvas.width - 32); //32 is the offset from the edge of the canvas
-        //this.yPosition = 0;
+        this.yPosition = randomChickenPosition.y + imageChicken.image.height;       
+        this.fallPosition = getRandomInt(randomChickenPosition.x, imagePlank.image.width); //TODO: This should become a calculation beween the length of the plank, the position of the chicken, and the direction of the egg to move.
+        this.rollSpeed = 1;
         this.speed = getRandomInt(5,10);
         this.angle = 0;
         this.caught = false;
@@ -17,8 +17,18 @@ class Egg{constructor(canvasContext, image, chickenPositions)
         if(!this.caught)
         {
             drawImageWithRotation(this.canvasContext, this.image, this.xPosition, this.yPosition, this.image.width / 2, this.image.height / 2, this.angle);
-            this.yPosition += this.speed;            
-            this.angle +=  (this.speed * Math.PI / 180);
+            
+            if(this.xPosition < this.fallPosition) //Roll on the plank
+            {
+                this.xPosition += this.rollSpeed;
+                this.angle += (this.rollSpeed * Math.PI / 180);
+            }
+            else //Fall down
+            {
+                this.yPosition += this.speed;            
+                this.angle +=  (this.speed * Math.PI / 180);
+            }
+            
         }
     }
 }
